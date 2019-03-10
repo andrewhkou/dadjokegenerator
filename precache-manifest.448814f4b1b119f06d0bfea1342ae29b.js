ea42a5bef3980f0d19e0616b0e31e04af9a06b72
Andrew Kou <andrewhkou@gmail.com>
2019-03-09 21:17:40 -0800

    Updates

---
+++ precache-manifest.448814f4b1b119f06d0bfea1342ae29b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "258fd8e6606d02ad5382",
+    "url": "/dadjokegenerator/static/js/runtime~main.b3f75bb8.js"
+  },
+  {
+    "revision": "d2498a7caefaab961fa1",
+    "url": "/dadjokegenerator/static/js/main.09f16bd2.chunk.js"
+  },
+  {
+    "revision": "fb2fc5b070a416bf4eb1",
+    "url": "/dadjokegenerator/static/js/2.99dfc4d0.chunk.js"
+  },
+  {
+    "revision": "d2498a7caefaab961fa1",
+    "url": "/dadjokegenerator/static/css/main.b0fd3a25.chunk.css"
+  },
+  {
+    "revision": "0bf5202bc45d1700c8f18d814376b925",
+    "url": "/dadjokegenerator/index.html"
+  }
+];
